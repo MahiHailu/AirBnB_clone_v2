8053b8f811465b3bbf9aefd77636d7b0ac350f9a
MahiHailu <hail.mulat@gmail.com>
2023-11-21 04:05:37 -0800

    airbab clone

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.place import Place
+
+
+class test_Place(test_basemodel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Place"
+        self.value = Place
+
+    def test_city_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.city_id), str)
+
+    def test_user_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.user_id), str)
